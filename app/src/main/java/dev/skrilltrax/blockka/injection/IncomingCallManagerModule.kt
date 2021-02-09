768a6b298ff8d00e7134c5213127a07e32b3f950
Aditya Wasan <adityawasan55@gmail.com>
2021-02-09 22:50:08 +0530

    src: replace deprecated ApplicationComponent with SingletonComponent
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- app/src/main/java/dev/skrilltrax/blockka/injection/IncomingCallManagerModule.kt
+++ app/src/main/java/dev/skrilltrax/blockka/injection/IncomingCallManagerModule.kt
@@ -5,11 +5,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import dev.skrilltrax.blockka.data.manager.IncomingCallManager
 
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 object IncomingCallManagerModule {
 
