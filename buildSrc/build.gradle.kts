10bb9b37aa524b0fb390cc088818567e66b2efbc
Aditya Wasan <adityawasan55@gmail.com>
2021-05-06 23:24:46 +0530

    build: downgrade to kotlin 1.4.32
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -20,6 +20,6 @@ gradlePlugin {
 dependencies {
   implementation("com.android.tools.build:gradle:7.0.0-alpha15")
   implementation("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
   implementation("com.squareup.sqldelight:gradle-plugin:1.5.0")
 }
